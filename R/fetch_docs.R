a45b1df7e21534d52ae2a9e219ed0d517e79f7ff
Paul C. Bauer <paulcbauer@users.noreply.github.com>
2018-04-09 20:59:58 +0200

    Various updates.

--- R/fetch_docs.R
+++ R/fetch_docs.R
@@ -1,70 +1,71 @@
-#' Fetches documents from paperpile gdrive folder and copies to folder.
+#' Fetches documents from paperpile gdrive folder and copies to folder (delete files if there are any in there).
 #'
-#' @param paperpile_dir The directory of the paperpile folder.
-#' @param folder folder...
-#' @param file file...
-#' @return Makes copies of relevant PDFs and stores them in folder.
+#' @param from The directory of the paperpile folder.
+#' @param to The folder to which you would like to copy the files.
+#' @param citations The dataframe that contains the citations, collected through the Web of Knowledge.
+#' @return Makes copies of relevant PDFs and stores them in "to" folder. Be careful, if the folder exists already it deletes it.
 
 
-fetch_docs <- function(paperpile_dir, folder, file) {
+fetch_docs <- function(from, to, citations) {
 
 
 
   # Create documents folder in respective study folder (or empty present one)
-  if(dir.exists(paste("./", folder, sep = ""))==TRUE){
-    unlink(paste("./", folder, sep = ""), recursive = T)} # delte folder
+  if(dir.exists(paste(to, sep = ""))==TRUE){
+    unlink(paste(to, sep = ""), recursive = T)} # delete files in folder
 
-  if(dir.exists(paste("./", folder, sep = ""))==FALSE){
-    dir.create(paste("./", folder, sep = ""))}
-
-
+  if(dir.exists(paste(to, sep = ""))==FALSE){ # create folder if it does not exist
+    dir.create(paste(to, sep = ""))}
 
 
 
-  x.default <- dir(paperpile_dir, recursive = T)
 
-  citations <- read.table(paste("./", file, sep=""), header=TRUE, stringsAsFactors = F)
+
+  x.default <- dir(from, recursive = T)
+
+
   # Loop to put PDF files in one folder
   # Finds PDF files in paperpile folder
   # Copies them to a new folder
 
   for (i in 1:nrow(citations)){
     if(stringr::str_detect(as.character(i), "[0-9]*0")){cat(i, ".. ", sep="")}
-    AU <- stringr::str_replace(stringr::str_extract(citations$author[i], "[:alpha:]*,"), ",", "")
-    PY <- citations$year[i]
-    TIT <- stringr::str_extract(citations$title[i], "[:word:]*\\s[:word:]*\\s[:word:]*\\s[:word:]*\\s[:word:]*\\s[:word:]*")
+    AU <- stringr::str_replace(stringr::str_extract(citations$AU[i], "[:alpha:]*,"), ",", "")
+    PY <- citations$PY[i]
+    TIT <- stringr::str_extract(citations$TI[i], "[:word:]*\\s[:word:]*\\s[:word:]*\\s[:word:]*\\s[:word:]*\\s[:word:]*")
 
 
 
-    # TEST
-      # x.default <- c("Trashed Papers/Rothgeb 1990 - INVESTMENT DEPENDENCE AND POLITICAL-CONFLICT IN THIRD-WORLD COUNTRIES.pdf",
-      #                "All Papers/Y/Young 2009 - State Capacity, Democracy, and the Violation of Personal Integrity Rights.pdf")
-      # AU <- "Rothgeb"
-      # PY <- "1990"
-      # TIT <- "Investment dependence"
 
-    # identify articles - only by year and first three words of title
     x <- x.default
     # x <- x[stringr::str_detect(x, stringr::coll(AU, TRUE))]
     # x <- x[stringr::str_detect(x, stringr::coll(PY, TRUE))]
-    x <- x[stringr::str_detect(x, stringr::coll(TIT, TRUE))]
+    # x <- x[stringr::str_detect(x, stringr::coll(TIT, TRUE))]
+    x <- x[stringr::str_detect(x, regex(TIT, ignore_case = T))] # ignore CAPS
+
+    if(length(x)>=10){ # only author and year
+      str <- c(AU, PY)
+      x <- x[apply(sapply(str, grepl, x), 1, all)] # ig
+      }
+
+
     file.path.half <- x
 
 
 
     if(length(file.path.half)!=0){
     # paperpile folder where file is
-    from <- paste(paperpile_dir, file.path.half, sep="")
+    from2 <- paste(from, "/", file.path.half, sep="")
 
-    to <- paste("./", folder, "/",
+    to2 <- paste(to, "/",
                 stringr::str_replace(file.path.half, "All Papers/[A-Z]/", ""), sep="")
-    file.copy(from, to, overwrite = T, recursive = FALSE,
+    file.copy(from2, to2, overwrite = T, recursive = FALSE,
               copy.mode = TRUE)
     }
 
 
   }
-  # cat("\n Wow.. copying took only ", as.numeric(time[3]), " second(s) for ", folder, ". Amazing!", sep = "")
+  # cat("\n Wow.. copying took only ", as.numeric(time[3]), " second(s) for ", to, ". Amazing!", sep = "")
   }
 
 
--- R/gen_ris.R
+++ R/gen_ris.R
@@ -6,38 +6,27 @@
 # file <- "Fearon 2003_citations-1001-1235.csv"
 # gen_ris(file)
 
-gen_ris <- function(dois, singlefile=TRUE){
+gen_ris <- function(dois, filename = "records.ris"){
 
     dois <- dois
 
-    if(singlefile==TRUE){
 
     length.dois <- length(dois)
 
-    fileConn <- file("./records.ris")
+    fileConn <- file(paste("./", filename, sep=""))
     writeLines(c(rbind(
 
       rep("TY  - JOUR", length.dois), # Journal article
+      #rep("AU  - NA", length.dois), # Journal article
+      #rep("TI  - NA", length.dois), # Journa
       paste("DO  - ", dois, sep=""), # DOI
-      rep("ER  -", length.dois)
+      rep("ER  - ", length.dois)
 
       )) # Separator
     , fileConn)
     close(fileConn)
 
-    }else{
 
-    dir.create("./ris-files/")
-    length.dois <- length(dois)
-    for (i in 1:length(dois)){ #
-      fileConn <- file(paste("./ris-files/", i, " - ", stringr::str_replace_all(dois[i], "[;\\<\\>:/]", ""), ".ris", sep=""))
-      writeLines(c("TY  - JOUR",                    # Journal article
-                   paste("DO  - ", dois[i], sep="") # DOI
-      ), fileConn)
-      close(fileConn)
-    }
-
-    }
 
 }
 
